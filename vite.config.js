c2207ef07ca8b267688b733b382e44c300c157e4
Felix Sargent <felix.sargent@gmail.com>
2025-12-10 19:59:31 +0000

    fix build warnings

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		port: 3000
+	},
+	build: {
+		rollupOptions: {
+			onwarn(warning, warn) {
+				// Suppress the @__PURE__ annotation warning from Rollup
+				if (warning.message.includes('@__PURE__')) {
+					return;
+				}
+				warn(warning);
+			}
+		}
 	}
 });
 
